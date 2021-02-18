9d0109a985570d22df65bd31a474885f399cf2f0
abdellah-khaddou <khaddou36@gmail.com>
2021-02-19 00:25:53 +0100

    g

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-
+let dataMaison;
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/client.html');
 });
 
 io.on('connection', (socket) => {
   console.log('a user connected');
-  socket.on('chat message', (msg) => {
+  socket.on('maison', (msg) => {
+    dataMaison = msg;
     console.log('message: ' + msg);
-    io.emit('chat message', msg);
+    io.emit('maison', msg);
   });
   socket.on('disconnect', () => {
     console.log('user disconnected');
